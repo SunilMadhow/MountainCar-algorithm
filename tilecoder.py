35927d36ed29a834862d0bfb5b9664fd071e0c29
Sunil Madhow <sunil.a.madhow@gmail.com>
2020-04-13 16:32:12 -0700

    finished rudimentary tile coding

--- tilecoder.py
+++ tilecoder.py
@@ -1,4 +1,5 @@
 import numpy as np
+from math import floor
 
 class Tilecoder:
 
@@ -10,23 +11,32 @@ class Tilecoder:
 		self.max_values = env.observation_space.high
 		self.min_values = env.observation_space.low
 
-		self.dim = len(self.max_values)
+		self.dim = len(self.max_values) #WLOG mountaincar statespace has a dimensionality of 2 (position and velocity)
 		self.actions = env.action_space
-		#length of weight vector
-		self.n = self.num_tiles * self.env.action_space.n
-		self.tile_size = np.divide(np.ones(self.dim), self.num_tiles -1)
 
-	
 
-	def features_to_tiles(self, features) {
-		#values is the array that represents the current observation
-		values = np.zeros(self.dim)
-		for i in range(self.dim):
-			values[i] = features[i]
-		tile_matrix = np.zeros([self.num_tilings, self.dim])
-		for i in range(self.num_tilings):
-			for j in range (self.dim):
-				tile_matrix[i,j] = int(values[j] / self.tile_size[j])
-	
+	def features_to_tiles(self, features):
+		one_hot = [0] * self.num_tiles * self.num_tilings * self.dim 
+		print(len(one_hot))
+		#a one_hot vector with precisely as many ones as there are tilings, with a one corrosponding to the data
+		#being present in that tile
 
-	}
+		for i in range(0, self.num_tilings):
+			for f in range(0, len(features)):
+				tile_span = (self.max_values[f] - self.min_values[f])/(self.num_tiles - 1)
+
+				offset = i * self.num_tiles / self.num_tilings
+				datum = features[f]
+				datum = datum + offset #to do -> optimize asymmetric staggering of tilings
+				datum = datum/tile_span
+				print ("x = ", x)
+				index = self.num_tiles*self.num_tilings*max(f-1, 0) + floor(datum)
+				print("index = {}", index)
+				one_hot[index] = 1
+
+		return one_hot
+
+	def print_vector(self, x, y):
+		vector = self.features_to_tiles([x,y])
+		print(vector)
+
