f0eeec6383d31215222dab99314f96b262440be3
Sunil Madhow <sunil.a.madhow@gmail.com>
2020-04-13 16:32:12 -0700

    finished rudimentary tile coding

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #implementing example 1 from Sutton 2nd Edition, Chapter 12
 
 import gym
-import tilecoder
+from tilecoder import Tilecoder	
 
 env = gym.make('MountainCar-v0')
+print(env.observation_space.high)
+print(env.observation_space.low)	
 
 tiler = Tilecoder(env, 4, 4)
-
+tiler.print_vector(-1, 0)
