56256b8136dd6f332067dcb69f0c5cc9e96f88ff
Sunil Madhow <sunil.a.madhow@gmail.com>
2020-08-05 13:14:58 -0700

    fixed several issues with tilecoder and made it (hopefully) more generalizable). Ran first successful attempt to solve mountaincar

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from tilecoder import Tilecoder
 
 np.seterr('ignore')
 
-env = gym.make('MountainCar-v0').env
+env = gym.make('MountainCar-v0')
 print(env.observation_space.high)
 print(env.observation_space.low)	
 print(env.action_space)
 
-tiler = Tilecoder(env, 8, 8)
+tiler = Tilecoder(env, 7, 14)
 # tiler.print_vector(-.569, 0, 2)
-tiler.print_vector(-.3, .07, 1)
-num_episodes = 500
+# tiler.print_vector(-.3, .07, 1)
+num_episodes = 100000000
 # w = [0] * tiler.vector_length #weight vector
 
 def Q(tiler, observation, w): #returns list of all q-values for the state
@@ -24,32 +24,41 @@ def Q(tiler, observation, w): #returns list of all q-values for the state
 		Q[i] = np.dot(tiler.tilecode(observation, i), w)
 	return Q
 
-
-alpha = .5/8 #stepsize
-w = np.zeros(tiler.vector_length) #parametrizing vector
-
+discount = 1
+alpha = .15/tiler.num_tilings  #stepsize
+w = np.zeros(tiler.vector_length)
+records = []
+# w = np.load('weights2.txt.npy') #parametrizing vector
+hundred_sum = 0
+G = 0
 for episode in range(num_episodes):
-	G = 0
+
+
 	observation = env.reset()
-	action = random.choice([0, 1, 2])
-	gradient = np.zeros(tiler.vector_length)
-	
+
+	action = np.argmax(Q(tiler, observation, w))
+
 	epsilon = 0
 	prediction = np.dot(tiler.tilecode(observation, action), w)
 	x_s = tiler.tilecode(observation, action)
+	G = 0
 	while True:
 		# env.render()
-
 		state2, reward, done, info = env.step(action)
+		G = G + reward
 		observation = state2
 
 		if done:
-			w  = w + (reward - prediction)*x_s
+			w  = w + alpha*(reward - prediction)*x_s
+			# print("final reward = {reward}".format(reward=reward))
 			observation = env.reset()
-			prevG = G
-			print ("Episode {ep} : Reward = {re}".format(ep = episode, re = G))
+			records.append(G)
+			# print ("Episode {ep} : Reward = {re}".format(ep = episode, re = G))
+			# np.save('weights2.txt', w)
+			# print(w)
 			break
 
+		action = 0
 		if random.random() > epsilon:
 			q_list = Q(tiler, observation, w)
 			action = np.argmax(q_list)
@@ -57,10 +66,33 @@ for episode in range(num_episodes):
 		else:
 			action = random.choice([0, 1, 2])
 		
-		G = G + reward
 		prime = np.dot(tiler.tilecode(observation, action), w)
-		delta = reward + prime - prediction
+		# print("prime = {p}".format(p=prime))
+		# print("timestep = {t}".format(t=G))
+		# print("prediction = {p}".format(p=prediction))
+		# print("reward = {p}".format(p=reward))
+		delta = reward + discount*prime - prediction
+		# print("delta = {d}".format(d = delta))
+		w_p = w
+		# print(w)
 		w = w + alpha*delta*x_s
+		# if np.any([i > 0 for i in w]):
+			# print("weight, index = {l}".format(l = [(i, w[i]) for i in range(0, len(w)) if w[i] > 0]))
+			# print(w_p)
+			# print("-------")
+			# print(w)
+
+			# exit()
 		# print(w)
 		x_s = tiler.tilecode(observation, action)
+
 		prediction = np.dot(tiler.tilecode(observation, action), w)
+
+	hundred_sum = hundred_sum + G
+
+	if episode % 100 == 0 and episode >= 100:
+		print("Average reward over 100 episodes = {k}".format(k = hundred_sum/100))
+		if sum(records[episode - 100:episode])/100 >= -110:
+				print("Solved in {d} episodes!".format(d = episode + 1))
+				break
+		hundred_sum = 0
--- tilecoder.py
+++ tilecoder.py
@@ -10,47 +10,52 @@ class Tilecoder:
 		#arrays containing the upper and lower bounds (respectively) of each observation 
 		self.max_values = env.observation_space.high
 		self.min_values = env.observation_space.low
+		self.high = self.max_values - self.min_values
+		self.low = self.min_values - self.min_values
 
-		self.dim = len(self.max_values) #WLOG mountaincar statespace has a dimensionality of 2 (position and velocity)
+		self.dim = len(self.max_values) 
 		self.actions = env.action_space
 		self.num_actions = 3
 
 		self.vector_length = self.num_tiles* self.num_tiles * self.num_tilings * self.num_actions
+		print("Output will be vector of length {leng}".format(leng = self.vector_length))
+
+		self.tile_span_x = (self.high[0] - self.low[0])/(self.num_tiles-1)
+		self.tile_span_y = (self.high[1] - self.low[1])/(self.num_tiles-1)
+		print("Tilespans are {spans}".format(spans=(self.tile_span_x, self.tile_span_y)))
 
 	def normalize(self, features): 
 		normalized = [0]*len(features)
 		for i in range(0, len(features)):
-			normalized[i] = (features[i] - self.min_values[i])/(self.max_values[i] - self.min_values[i])
+			normalized[i] = (features[i] - self.min_values[i])
 		return normalized
 
 	def tilecode(self, raw_features, action):
+		# print("-------CALL TO TILECODE------------")
+		# print("action = {act}".format(act = action))
 		one_hot = np.zeros(self.num_tiles* self.num_tiles * self.num_tilings * self.num_actions)
 		# print("featuers from inside fcall = ", raw_features)
 		features = self.normalize(raw_features)
 		for i in range(1, self.num_tilings+1):
-			
-			tile_span = 1/(self.num_tiles-1)
-			# print("tile_span = ", tile_span)
-			offset_x = i * tile_span / self.num_tilings
-			offset_y = 1.5 * offset_x
+			# print("Features: {f}".format(f = features))
+
+			offset_x = (i-1) * self.tile_span_x / self.num_tilings
+			offset_y = (i -1) * self.tile_span_y * 1.03/ self.num_tilings
 			
 			x = features[0] + offset_x
 			y = features[1] + offset_y 
-			
-			# print("x = ", x)
-			# print("y = ", y)
+			# print("offset data = {tup}".format(tup = (x, y)))
 
-			n = floor((x /tile_span)+1)
-			j = floor((y /tile_span)+1)
+			n = min(floor((x /self.tile_span_x)+1), self.num_tiles)
+			j = min(floor((y /self.tile_span_y)+1), self.num_tiles)
+			# print("Tiles for offset = {tils}".format(tils = (n, j)))
 
-			position = n + 4*(j-1)
-			# print("n =", floor(n))
-			# print("j = ", floor(j))
-			# print("plosition = ", floor(position))
+			position = n + self.num_tiles*(j-1)
+			# print("Index within tile = {inderx}".format(inderx = position))
 			
-			index = (int)(self.num_tiles*self.num_tiles*(i-1)*self.num_actions + floor(position) + action)
-			# print(index)
-			one_hot[index] = 1
+			index = (int)(self.num_tiles*self.num_tiles*(i-1)*self.num_actions + int(position) + action*self.num_tiles*self.num_tiles)
+			# print("index = {ind}".format(ind = index))
+			one_hot[index -1] = 1
 
 		return one_hot
 
