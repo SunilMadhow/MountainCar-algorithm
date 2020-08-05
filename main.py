e24d430fab1f0fc17545276a53c7a70190c34caa
Sunil Madhow <sunil.a.madhow@gmail.com>
2020-08-05 13:23:33 -0700

    cleaned up formatting

--- main.py
+++ main.py
@@ -16,19 +16,17 @@ tiler = Tilecoder(env, 7, 14)
 # tiler.print_vector(-.569, 0, 2)
 # tiler.print_vector(-.3, .07, 1)
 num_episodes = 100000000
-# w = [0] * tiler.vector_length #weight vector
 
-def Q(tiler, observation, w): #returns list of all q-values for the state
+def Q(tiler, observation, w): #returns list of all q-values for the state over all actions
 	Q = np.zeros(tiler.num_actions)
 	for i in range(tiler.num_actions):
 		Q[i] = np.dot(tiler.tilecode(observation, i), w)
 	return Q
 
 discount = 1
-alpha = .15/tiler.num_tilings  #stepsize
+alpha = .32/tiler.num_tilings  
 w = np.zeros(tiler.vector_length)
 records = []
-# w = np.load('weights2.txt.npy') #parametrizing vector
 hundred_sum = 0
 G = 0
 for episode in range(num_episodes):
@@ -50,12 +48,8 @@ for episode in range(num_episodes):
 
 		if done:
 			w  = w + alpha*(reward - prediction)*x_s
-			# print("final reward = {reward}".format(reward=reward))
 			observation = env.reset()
 			records.append(G)
-			# print ("Episode {ep} : Reward = {re}".format(ep = episode, re = G))
-			# np.save('weights2.txt', w)
-			# print(w)
 			break
 
 		action = 0
@@ -67,25 +61,9 @@ for episode in range(num_episodes):
 			action = random.choice([0, 1, 2])
 		
 		prime = np.dot(tiler.tilecode(observation, action), w)
-		# print("prime = {p}".format(p=prime))
-		# print("timestep = {t}".format(t=G))
-		# print("prediction = {p}".format(p=prediction))
-		# print("reward = {p}".format(p=reward))
 		delta = reward + discount*prime - prediction
-		# print("delta = {d}".format(d = delta))
-		w_p = w
-		# print(w)
 		w = w + alpha*delta*x_s
-		# if np.any([i > 0 for i in w]):
-			# print("weight, index = {l}".format(l = [(i, w[i]) for i in range(0, len(w)) if w[i] > 0]))
-			# print(w_p)
-			# print("-------")
-			# print(w)
-
-			# exit()
-		# print(w)
 		x_s = tiler.tilecode(observation, action)
-
 		prediction = np.dot(tiler.tilecode(observation, action), w)
 
 	hundred_sum = hundred_sum + G
--- tilecoder.py
+++ tilecoder.py
@@ -31,30 +31,17 @@ class Tilecoder:
 		return normalized
 
 	def tilecode(self, raw_features, action):
-		# print("-------CALL TO TILECODE------------")
-		# print("action = {act}".format(act = action))
 		one_hot = np.zeros(self.num_tiles* self.num_tiles * self.num_tilings * self.num_actions)
-		# print("featuers from inside fcall = ", raw_features)
 		features = self.normalize(raw_features)
-		for i in range(1, self.num_tilings+1):
-			# print("Features: {f}".format(f = features))
-
+		for i in range(1, self.num_tilings+1):			
 			offset_x = (i-1) * self.tile_span_x / self.num_tilings
-			offset_y = (i -1) * self.tile_span_y * 1.03/ self.num_tilings
-			
+			offset_y = (i -1) * self.tile_span_y * 1.03/ self.num_tilings			
 			x = features[0] + offset_x
 			y = features[1] + offset_y 
-			# print("offset data = {tup}".format(tup = (x, y)))
-
 			n = min(floor((x /self.tile_span_x)+1), self.num_tiles)
 			j = min(floor((y /self.tile_span_y)+1), self.num_tiles)
-			# print("Tiles for offset = {tils}".format(tils = (n, j)))
-
 			position = n + self.num_tiles*(j-1)
-			# print("Index within tile = {inderx}".format(inderx = position))
-			
 			index = (int)(self.num_tiles*self.num_tiles*(i-1)*self.num_actions + int(position) + action*self.num_tiles*self.num_tiles)
-			# print("index = {ind}".format(ind = index))
 			one_hot[index -1] = 1
 
 		return one_hot
